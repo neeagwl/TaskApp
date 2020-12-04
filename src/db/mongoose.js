1bc828767e9e76807ec573ad2e5f9716b9db6dd4
Agarwal Neetu <agarwalneetu301@gmail.com>
2020-12-04 18:21:42 +0530

    Authentication Add

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,4 @@
+// "C:\Program Files\MongoDB\Server\4.4\bin\mongo.exe"
 const mongoose=require("mongoose");
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-app-api',{
--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 // "C:\Program Files\MongoDB\Server\4.4\bin\mongo.exe"
-const express= require("express");
-require('./db/mongoose');
-const userRouter= require("./router/user");
-const taskRouter = require("./router/task");
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+const Task =require("./models/task");
+const User = require('./models/user');
 
-const app=express();
-const port=process.env.PORT  || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
+const main= async ()=>{
+    // const task =  await Task.findById('5fc4a586cf46cf09f883cdae');
+    // await task.populate('owner').execPopulate();
+    // console.log(task.owner);
+    const user = await User.findById('5fc4a37d79d6990d6c960490');
+    await user.populate('tasks').execPopulate();
+    console.log(user.tasks)
+}
 
-app.get("/",(req,res)=>{
-    res.send("hello")
-});
+// main();
 
-app.listen(port,()=>{
-    console.log(`Server started on ${port}`);
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
